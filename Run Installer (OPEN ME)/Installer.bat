de09aafdb2bdac86cafa275b6806085b81b2efd3
Tx998 <lil.t998667@gmail.com>
2025-03-13 19:24:12 +0100

    Add files via upload

---
+++ Run Installer (OPEN ME)/Installer.bat
@@ -0,0 +1,8 @@
+@echo off
+echo [92mInstalling required Python modules...[0m
+pip install PyQt5
+pip install pywin32
+pip install psutil
+pip install requests
+echo [92mAll modules installed successfully![0m
+pause
